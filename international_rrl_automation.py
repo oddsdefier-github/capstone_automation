445253994599ebb7738f46a9a8697dba41b24275
Odds <panerjeffryjames@gmail.com>
2023-06-22 09:18:38 +0800

    it

--- international_rrl_automation.py
+++ international_rrl_automation.py
@@ -4,37 +4,40 @@ import time
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-links_international = [
-    "https://www.linkedin.com/pulse/can-human-errors-avoided-business-processes-patrick-mutabazi/",
-    "https://ieeexplore.ieee.org/document/7530621",
-    "https://www.comidor.com/blog/business-process-management/a-comparison-of-manual-and-automated-workflow-processes/",
-    "https://www.researchgate.net/publication/369235619_Automatic_Energy_Meter_Billing_System_with_Theft_Detection",
-    "https://fepbl.com/index.php/ijarss/article/view/440",
-    "http://hdl.handle.net/11394/9216",
-    "https://ieeexplore.ieee.org/document/8960144",
-    "https://www.researchgate.net/publication/345776202_Implementation_of_Improved_Billing_System",
-    "https://ieeexplore.ieee.org/document/9835742",
-    "https://ieeexplore.ieee.org/abstract/document/8597179",
-    "https://ieeexplore.ieee.org/abstract/document/9675569",
-    "https://ieeexplore.ieee.org/abstract/document/9889267",
-    "https://ieeexplore.ieee.org/document/9442031",
-    "https://www.proquest.com/openview/0d88a38ba77f360ea4e504e1ebc76e2f/1.pdf?pq-origsite=gscholar&cbl=1976354",
-    "https://ieeexplore.ieee.org/document/8339088",
-    "https://www.sciencedirect.com/science/article/pii/S1877042811024621",
-    "https://thesai.org/Downloads/Volume12No8/Paper_48-An_Efficient_IoT_Based_Smart_Water_Meter_System.pdf",
-    "https://arxiv.org/abs/2106.15005",
-    "https://www.jetir.org/view?paper=JETIR2106786",
-    "https://www.calendar.com/blog/data-visualization-is-the-key-to-team-productivity/"
-]
+links_international = {
+    "1": "https://www.linkedin.com/pulse/can-human-errors-avoided-business-processes-patrick-mutabazi/",
+    "2": "https://ieeexplore.ieee.org/document/7530621",
+    "3": "https://www.comidor.com/blog/business-process-management/a-comparison-of-manual-and-automated-workflow-processes/",
+    "4": "https://www.researchgate.net/publication/369235619_Automatic_Energy_Meter_Billing_System_with_Theft_Detection",
+    "5": "https://fepbl.com/index.php/ijarss/article/view/440",
+    "6": "http://hdl.handle.net/11394/9216",
+    "7": "https://ieeexplore.ieee.org/document/8960144",
+    "8": "https://www.researchgate.net/publication/345776202_Implementation_of_Improved_Billing_System",
+    "9": "https://ieeexplore.ieee.org/document/9835742",
+    "10": "https://ieeexplore.ieee.org/abstract/document/8597179",
+    "11": "https://ieeexplore.ieee.org/abstract/document/9675569",
+    "12": "https://ieeexplore.ieee.org/abstract/document/9889267",
+    "13": "https://ieeexplore.ieee.org/document/9442031",
+    "14": "https://www.proquest.com/openview/0d88a38ba77f360ea4e504e1ebc76e2f/1.pdf?pq-origsite=gscholar&cbl=1976354",
+    "15": "https://ieeexplore.ieee.org/document/8339088",
+    "16": "https://www.sciencedirect.com/science/article/pii/S1877042811024621",
+    "17": "https://thesai.org/Downloads/Volume12No8/Paper_48-An_Efficient_IoT_Based_Smart_Water_Meter_System.pdf",
+    "18": "https://arxiv.org/abs/2106.15005",
+    "19": "https://www.jetir.org/view?paper=JETIR2106786",
+    "20": "https://www.calendar.com/blog/data-visualization-is-the-key-to-team-productivity/"
+}
 
-driver.get(links_international[0])
 
-for index, link in enumerate(links_international[1:], 1):
+driver.get(links_international["1"])
+time.sleep(3)
+driver.execute_script("return window.stop();")
+
+for index, link in list(links_international.items())[1:]:
     driver.execute_script(f"window.open('{link}', '_blank');")
-    print(f'[{index}] {link}')
+    print(f"[{index}] {link}")
     driver.switch_to.window(driver.window_handles[-1])
-    time.sleep(3)  # Wait for 3 seconds
-    driver.execute_script("window.stop();")
+    time.sleep(3)
+    driver.execute_script("return window.stop();")
 
 input("Press any key to exit...")
 driver.quit()
--- local_rrl_automation.py
+++ local_rrl_automation.py
@@ -1,38 +1,42 @@
+import time
 from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-links_local = [
-    "https://ieeexplore.ieee.org/abstract/document/9815483/authors#authors",
-    "https://www.researchgate.net/publication/321091817_Information_systems_for_smart_services",
-    "https://journalppw.com/index.php/jpsp/article/view/7263",
-    "https://ejournals.ph/article.php?id=12127",
-    "https://ejournals.ph/article.php?id=6120",
-    "https://ejournals.ph/article.php?id=6121",
-    "https://www.pids.gov.ph/publication/discussion-papers/the-philippine-local-government-water-sector",
-    "https://www.pids.gov.ph/publication/philippine-journal-of-development/an-assessment-of-the-financial-sustainability-and-performance-of-philippine-water-districts",
-    "https://www.researchgate.net/publication/326260033_Web-based_Billing_and_Collection_System_for_a_Municipal_Water_and_Services_Unit",
-    "https://journal.evsu.edu.ph/index.php/itmj/article/view/41",
-    "https://www.ijres.org/papers/Volume-10/Issue-6/100610871091.pdf",
-    "https://ejournals.ph/article.php?id=7261",
-    "https://ieeexplore.ieee.org/abstract/document/9977678",
-    "https://ieeexplore.ieee.org/abstract/document/6804531",
-    "https://research.lpubatangas.edu.ph/wp-content/uploads/2015/05/APJARBA-2015-1-006-Computerized-vs.-Non-computerized-Accounting-System.pdf",
-    "https://www.sajst.org/online/index.php/sajst/article/view/130",
-    "https://dl.acm.org/doi/abs/10.1145/3421682.3421691",
-    "https://payo.asia/ecommerce-automation-philippines/",
-    "https://www.sciencedirect.com/science/article/pii/S1877050919318149",
-    "https://ieeexplore.ieee.org/abstract/document/8269446"
-    
-]
+links_local = {
+    "2": "https://ieeexplore.ieee.org/abstract/document/9815483/authors#authors",
+    "3": "https://www.researchgate.net/publication/321091817_Information_systems_for_smart_services",
+    "4": "https://journalppw.com/index.php/jpsp/article/view/7263",
+    "5": "https://ejournals.ph/article.php?id=12127",
+    "6": "https://ejournals.ph/article.php?id=6120",
+    "7": "https://ejournals.ph/article.php?id=6121",
+    "8": "https://www.pids.gov.ph/publication/discussion-papers/the-philippine-local-government-water-sector",
+    "9": "https://www.pids.gov.ph/publication/philippine-journal-of-development/an-assessment-of-the-financial-sustainability-and-performance-of-philippine-water-districts",
+    "10": "https://www.researchgate.net/publication/326260033_Web-based_Billing_and_Collection_System_for_a_Municipal_Water_and_Services_Unit",
+    "11": "https://journal.evsu.edu.ph/index.php/itmj/article/view/41",
+    "12": "https://www.ijres.org/papers/Volume-10/Issue-6/100610871091.pdf",
+    "13": "https://ejournals.ph/article.php?id=7261",
+    "14": "https://ieeexplore.ieee.org/abstract/document/9977678",
+    "15": "https://ieeexplore.ieee.org/abstract/document/6804531",
+    "16": "https://research.lpubatangas.edu.ph/wp-content/uploads/2015/05/APJARBA-2015-1-006-Computerized-vs.-Non-computerized-Accounting-System.pdf",
+    "17": "https://www.sajst.org/online/index.php/sajst/article/view/130",
+    "18": "https://dl.acm.org/doi/abs/10.1145/3421682.3421691",
+    "19": "https://payo.asia/ecommerce-automation-philippines/",
+    "20": "https://www.sciencedirect.com/science/article/pii/S1877050919318149",
+    "21": "https://ieeexplore.ieee.org/abstract/document/8269446"
+}
 
-driver.get(links_local[0])
+driver.get(list(links_local.values())[0])
+time.sleep(3)
+driver.execute_script("window.stop();")
 
-for index, link in enumerate(links_local[1:], 1):
+for index, link in list(links_local.items())[1:]:
     driver.execute_script(f"window.open('{link}', '_blank');")
     print(f'[{index}] {link}')
     driver.switch_to.window(driver.window_handles[-1])
+    time.sleep(3)
+    driver.execute_script("window.stop();")
 
 input("Press any key to exit...")
 driver.quit()
